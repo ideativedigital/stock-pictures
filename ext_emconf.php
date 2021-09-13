f7ec134124f735c92290aa62b57e49bd27afd6e8
Bertrand Zang <bertrandzang@Bertrands-MacBook-Pro.local>
2021-09-13 17:06:53 +0200

    CHUVCAT21AP-30 init v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '10.0.5',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.9.99'
+            'typo3' => '11.0.0-11.9.99'
         ],
         'conflicts' => [
         ],
