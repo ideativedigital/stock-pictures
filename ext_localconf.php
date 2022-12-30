3537f198bc1657843e1673e689c99c52812f3e14
Mario Lubenka <mario.lubenka@googlemail.com>
2022-12-30 15:48:07 +0100

    [TASK] Fix button rendering for TYPO3 v12

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,8 +6,8 @@ call_user_func(function ($extKey) {
     /**
      * XClass the inline container to add a new button "Add media" for each connected service (Shutterstock, Unsplash, etc.)
      */
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][ \TYPO3\CMS\Backend\Form\Container\InlineControlContainer::class ] = array(
-        'className' => \Ideative\IdStockPictures\XClass\InlineControlContainer::class,
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][ \TYPO3\CMS\Backend\Form\Container\FilesControlContainer::class ] = array(
+        'className' => \Ideative\IdStockPictures\XClass\FilesControlContainer::class,
     );
 
 }, 'id_stock_pictures');
